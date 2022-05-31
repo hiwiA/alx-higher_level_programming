ea6c97b1ea2f7a60614ce4276628b37f7b0cc479
hiwiA <hiwotameha@gmail.com>
2022-05-31 11:01:03 -0700

    a

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if(i is 8 and j is 9):
-            print("{}".format(str(i) + str(j)))
-        elif(j > i):
-            print("{}".format(str(i) + str(j)) + ", ", end="")
+for a in range(0, 8):
+    for b in range(a + 1, 10):
+        print("{:d}{:d}".format(a, b), end=", ")
+print("{:d}{:d}".format(a + 1, b))
