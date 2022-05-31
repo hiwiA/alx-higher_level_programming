577e7aeeb30a8f449e3ced81622c39443c071507
hiwiA <hiwotameha@gmail.com>
2022-05-31 08:16:18 -0700

    d

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print("{}\n{}\n{}\n{}\n".format(str, str, str, str[0:9]))
+print("{}{}{}".format(str, str, str))
+str = str[0:9]
+print("{}".format(str))
