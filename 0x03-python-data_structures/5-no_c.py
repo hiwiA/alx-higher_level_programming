a8bece10b55f3d4471cabbc9e08fc2f411d8e22c
hiwiA <hiwotameha@gmail.com>
2022-06-04 11:41:01 -0700

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new += i
-    return new
+    l = list(my_string)
+    for x in l:
+        if x == 'c':
+            l.remove('c')
+        if x == 'C':
+            l.remove('C')
+        my_string = "".join(l)
+        return my_string
