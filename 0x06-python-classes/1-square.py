7a9f2fba25c0b5a35213281754b0c37bbf85a180
Hiwot Ameha <hiwotameha23@gmail.com>
2022-06-21 21:31:04 +0300

    t

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+""" Creates an empty class called Square
+"""
+
+
 class Square:
-    """
-    creates a square class object
+    """ Empty class with size private attribute
     """
     def __init__(self, size):
+        """
+                Instantiation with size
+        Args:
+            size: size of the square
+        """
         self.__size = size
-    """
-    initializes a square object with parameters
-    args:
-        __size(int): size of the square
-    """
