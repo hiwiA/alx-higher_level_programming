25f095860140c3d086a5a647a76a664368255e2b
Hiwot Ameha <hiwotameha23@gmail.com>
2022-06-21 20:58:07 +0300

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,14 +4,13 @@
 
 
 class Square:
-        """ Empty class with size private attribute
-            """
-                def __init__(self, size):
-                            """
-                                            Instantiation with size
-                                                    Args:
-                                                                size: size of the square
-                                                                        """
-                                                                                self.__size = size
-
+    """ Empty class with size private attribute
+    """
+    def __init__(self, size):
+        """
+                Instantiation with size
+        Args:
+            size: size of the square
+        """
+        self.__size = size
 
