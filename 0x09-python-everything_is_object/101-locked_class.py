67b0c246af5da719c7bd27b8e8cca05e592d47ce
Hiwot Ameha <hiwotameha23@gmail.com>
2022-06-28 21:35:53 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
+
 class LockedClass:
-    """ Class with no attributes """
-    def __setattr__(self, name, value):
-        """ Called when an attribute assignment is attempted."""
-        if name == "first_name":
-            self.__dict__[name] = value
-        else:
-            msg = "'LockedClass' object has no attribute '" + name + "'"
-            raise AttributeError(msg)
+    __slots__ = ['first_name']
