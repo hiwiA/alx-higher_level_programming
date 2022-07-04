3976377285a3478f6490432beb8ccfe095bce1f1
Hiwot Ameha <hiwotameha23@gmail.com>
2022-07-04 21:50:15 +0300

    task

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""class mylist"""
+''' Module: 1-my_list
+'''
 
 
 class MyList(list):
-    """prints sorted list of class"""
+    ''' Represents a MyList
+    '''
+
     def print_sorted(self):
+        '''
+        prints the list, but sorted
+        '''
         print(sorted(self))
