c48e11dde79655788ac1a8d95872cf6e308f064c
Hiwot Ameha <hiwotameha23@gmail.com>
2022-07-05 14:57:45 +0300

    task

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module Student  """
+
+
+class Student:
+    """ Student Class """
+    def __init__(self, first_name, last_name, age):
+        """ Init method """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return class in JSON"""
+        if attrs is None:
+            return self.__dict__
+
+        name_attrs = {}
+        for key, val in self.__dict__.items():
+            if key in attrs:
+                name_attrs[key] = val
+        return name_attrs
