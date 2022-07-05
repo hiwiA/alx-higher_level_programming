6b5739e9fe9c7fed1c1eab0b47a96089333f253b
Hiwot Ameha <hiwotameha23@gmail.com>
2022-07-05 15:03:56 +0300

    task

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-""" module pascal """
+"""pascal triangle"""
 
 
 def pascal_triangle(n):
-    """ return a list """
-    pascal_list = []
-    if n <= 0:
-        return pascal_list
-
-    for line in range(1, n + 1):
-        num = 1
-        p_list = []
-        for i in range(1, line + 1):
-            p_list.append(str(num))
-            num = int(num * (line - i) / i)
-        pascal_list.append(p_list)
-    return pascal_list
+    """does it"""
+    Triangle = []
+    prevN = []
+    for i in range(n):
+        atN = [1] + [sum(prevN[ele:ele + 2]) for ele in range(len(prevN) - 1)]
+        if i > 0:
+            atN = atN + [1]
+        Triangle.append(atN)
+        prevN = atN
+    return Triangle
