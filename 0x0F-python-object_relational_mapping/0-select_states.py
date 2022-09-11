24244b9c2da9d140553e1de41d0d126507b4c9f9
Hiwot Ameha <hiwotameha23@gmail.com>
2022-09-11 22:15:20 +0300

    task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
 """
-Module States
-script that lists all states from the database hbtn_0e_0_usa
+This script lists all states from the
+database `hbtn_0e_0_usa`.
 """
-from sys import argv
+
 import MySQLdb
+from sys import argv
+
 if __name__ == '__main__':
-    MY_USER = argv[1]
-    MY_PASSWD = argv[2]
-    MY_DB = argv[3]
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    '''Open Database connection'''
-    mydb = MySQLdb.connect(host="localhost",
-                           user=MY_USER,
-                           passwd=MY_PASSWD,
-                           db=MY_DB)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
 
-    '''Create cursor for operate over DB'''
-    myCursor = mydb.cursor()
-
-    '''pass and execute a SQL sentence'''
-    myCursor.execute("select * from states \
-                     order by states.id")
-
-    '''retrive records and fill cursor'''
-    states = myCursor.fetchall()
-
-    '''Print rows'''
-    for state in states:
-        print(state)
-
-    '''close cursor'''
-    myCursor.close()
-
-    '''close connection to DB'''
-    mydb.close()
+    for row in rows:
+        print(row)
