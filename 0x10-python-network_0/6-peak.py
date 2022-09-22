5a6ece6a8df5d5ab1096090cfa839f95d1494743
Hiwot Ameha <hiwotameha23@gmail.com>
2022-09-22 22:26:52 +0300

    task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
-""" find peak in a list """
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
+    """Return a peak in a list of unsorted integers."""
     if list_of_integers == []:
         return None
-    if len(list_of_integers) == 1:
+
+    size = len(list_of_integers)
+    if size == 1:
         return list_of_integers[0]
-    if len(list_of_integers) == 2:
+    elif size == 2:
         return max(list_of_integers)
 
-    _list = list_of_integers
-    mid = int(len(_list) / 2)
-    length = len(_list)
-    if (_list[mid - 1] <= _list[mid]) and (_list[mid + 1] <= _list[mid]):
-        return _list[mid]
-    elif _list[mid] < _list[mid + 1]:
-        return find_peak(_list[mid + 1:])
-        # return peak(array, mid + 1, high)
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        # return peak(array, low, mid - 1)
-        return find_peak(_list[:mid - 1])
-    # return peak(list_of_integers, 0, len(list_of_integers) - 1)
-
-# not used
-'''
-def peak(array, low, high):
-    mid = int((low + high) / 2)
-    l = len(array)
-    if (array[mid - 1] <= array[mid]) and (array[mid + 1] <= array[mid]):
-        return array[mid]
-    elif array[mid] < array[mid + 1]:
-        return peak(array, mid + 1, high)
-    else:
-        return peak(array, low, mid - 1)
-'''
+        return find_peak(list_of_integers[mid + 1:])
