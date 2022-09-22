8aa43ccf72a2372b3703337cf4522eaf2a999e8e
Hiwot Ameha <hiwotameha23@gmail.com>
2022-09-22 22:27:59 +0300

    task

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI "$1" | grep -Fi 'HTTP' | cut -d ' ' -f2- | cut -d ' ' -f1
+# This program sends a request to a URL passed as an argument, and displays only the status code of the response.
+curl -s -o /dev/null -w '%{http_code}' "$1"
