5b02b3c4f61930de2adf2a304433fb08bd8b8d70
Hiwot Ameha <hiwotameha23@gmail.com>
2022-09-23 20:39:46 +0300

    task

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-#  sends a request to the URL and displays the value of
-# the variable X-Request-Id in the response header
-""" script that fetches https://intranet.hbtn.io/status """
-
+"""
+takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
+"""
 import requests
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-        url = sys.argv[1]
-        payload = {'email': sys.argv[2]}
-        r = requests.post(url, data=payload)
-        print(r.text)
+if __name__ == '__main__':
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
+    print(r.text)
